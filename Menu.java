4fd6f5d4f278c89ac3d18e9be45ff4a9c603c34f
MAIRA FERNANDES NUNES <143126898+MairaFN@users.noreply.github.com>
2024-06-11 01:31:08 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -2,8 +2,9 @@ package RPGp2;
 
 public interface Menu {
     void logo();
+    void pausa();
+    char Ler();
     Personagem criar();
     void referencias();
     void mostrarAtributos();
-    void escolha();
 }
